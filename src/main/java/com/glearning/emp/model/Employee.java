c1c636fd037c325da1eda581d960fc33a1e989f2
pradeep <pradeep.kumar44@gmail.com>
2023-01-08 17:34:49 +0530

    committing the changes to the employee servcie and employee JPA repository

--- src/main/java/com/glearning/emp/model/Employee.java
+++ src/main/java/com/glearning/emp/model/Employee.java
@@ -1,9 +1,12 @@
 package com.glearning.emp.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name="employees")
 public class Employee {
 	
 	@Id
