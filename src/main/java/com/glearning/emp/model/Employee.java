7f24c50999afe51e8360420c65f89da46b055d32
pradeep <pradeep.kumar44@gmail.com>
2023-01-08 17:21:53 +0530

    committing the model class

---
+++ src/main/java/com/glearning/emp/model/Employee.java
@@ -0,0 +1,29 @@
+package com.glearning.emp.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+}
