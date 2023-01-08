8703f56587b6aae9b67261af98849ba1f6ada375
pradeep <pradeep.kumar44@gmail.com>
2023-01-08 18:00:29 +0530

    committing the changes

--- src/main/java/com/glearning/emp/controller/EmployeeController.java
+++ src/main/java/com/glearning/emp/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.glearning.emp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.glearning.emp.service.EmployeeService;
 
@@ -17,8 +18,21 @@ public class EmployeeController {
 	
 	@GetMapping("/employees")
 	public String listemployees(Model model) {
+		//hash map. key will be "employees" and value will be a list of all the employees
 		model.addAttribute("employees", this.employeeService.getAllEmployees());
 		return "employees";
+	}
+	
+	@GetMapping("/employees/edit/{id}")
+	public String editemployeeForm(@PathVariable Long id, Model model) {
+		model.addAttribute("employee", employeeService.getEmployeeById(id));
+		return "edit_employee";
+	}
+	
+	@GetMapping("/employees/{id}")
+	public String deleteemployee(@PathVariable Long id) {
+		employeeService.deleteEmployeeById(id);
+		return "redirect:/employees";
 	}
 
 }
